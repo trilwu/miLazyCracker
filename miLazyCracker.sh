97dc8914f21ca0ba292335a6ebab440c77cec9c4
Joan Bono <joan@bono.cat>
2017-11-30 23:28:25 +0100

    Update miLazyCracker.sh
    Now it works as expected.

--- miLazyCracker.sh
+++ miLazyCracker.sh
@@ -116,7 +116,7 @@ rm -f "$TMPFILE_UNK" "$TMPFILE_FND" "0x${myUID}_"*".txt"
 if [ $mfocResult -eq 0 ]; then
     echo -e "\n\nDump left in: $TMPFILE_MFD"
     if ask "Do you want clone the card? Place card on reader now and press Y"; then
-         nfc-mfclassic W a $TMPFILE_MFD
+         nfc-mfclassic w A u $TMPFILE_MFD
     fi
 else
     rm -f "$TMPFILE_MFD"
